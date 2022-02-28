75391196bbb9a3dbda95571655e1a1874c637cb1
Luis <luis_nicolas_martinez_tsapp1ma2021@cev.com>
2022-02-28 19:10:15 +0100

    middleware administrador

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
+            $table->enum('rol', ['Particular', 'Profesional', 'Administrador'])->default('Administrador');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
